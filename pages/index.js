f4673f24fb2dd54c6b55123d984203aa3b6ca39b
Calin66 <62961099+Calin66@users.noreply.github.com>
2023-04-28 23:33:15 +0300

    sliderVideo in my tech, tmr in ts and sass

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,197 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Roboto } from "next/font/google";
+import { useEffect, useRef, useState } from "react";
+import { AnimatePresence, animate, motion } from "framer-motion";
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+const buttonsUpSlider = [
+  { title: "Code editor", icon: "/icons/icons8-source-code-64" },
+  { title: "Browser", icon: "/icons/icons8-dollar-64" },
+  { title: "Visual CSS selectors", icon: "/icons/dotted-square" },
+  { title: "Queue", icon: "/icons/icons8-play-100" },
+  { title: "Logs", icon: "/icons/icons8-file-104" },
+  { title: "Storage", icon: "/icons/icons8-grid-64" },
+];
+
+const sliderElements = [
+  {
+    title:
+      "Revolutionising workflow development with a powerful JavaScript code editor",
+    desc: "Our editor is designed to simplify data automation and web scraping workflows development. With a high-level SDK, developers can control and interact with any web page, API, or database, making automation and data scraping fast and easy. ",
+    img: "/img/leftImage.png",
+  },
+  {
+    title: "Build powerful automation workflows with ad-free headless Chrome",
+    desc: "Our headless Chrome browser with integrated ad-blocking makes it easy for developers to build automation workflows in one place. With distraction-free browsing, developers can test and debug automation workflows directly within the app. Perfect for web scraping or browser automation projects.",
+    img: "/img/leftImage-1.png",
+  },
+  {
+    title: "Simplify workflow development with CSS selector feature",
+    desc: "Our CSS selector feature simplifies web automation and web scraping workflows by making it easier for developers to find DOM elements on a web page. With this tool, developers can easily and quickly target specific elements, reducing the time and effort needed for development. Whether you're building a web scraper or automating a repetitive task, our CSS selector feature is a must-have tool for any developer.",
+    img: "/img/vcss_img.png",
+  },
+  {
+    title:
+      "Enhance your web automation with a visual and filterable task queue",
+    desc: "Supercharge your web automation workflow with our visual and filterable task queue. Get all the essential details you need, including task status, creation date, arguments, and more - all available in table and JSON formats. Say goodbye to slow and cumbersome processes and get your tasks completed with lightning speed.",
+    img: "/img/leftImage-2.png",
+  },
+  {
+    title:
+      "Optimise web automation with advanced log filtering for efficient debugging",
+    desc: "Our log filtering feature is a must-have for efficient web automation and web scraping workflows. With the ability to quickly filter logs by log level, date, task name, and log message, developers can easily debug, test, and troubleshoot their workflows. This streamlined approach saves time and effort while ensuring high-quality results. Upgrade your workflow with our log filtering feature today.",
+    img: "/img/leftImage-3.png",
+  },
+  {
+    title:
+      "Maximise web automation efficiency with easy data storage and management",
+    desc: "Boost your web automation workflow with easy data storage and management. This feature supports popular databases like MongoDB, PostgreSQL, MySQL, Elasticsearch, Redis, as well as file storage systems like , CSV, S3, and more. Efficiently store and organise your data using advanced sorting and filtering options to view it in table or JSON format.",
+    img: "/img/leftImage-4.png",
+  },
+];
+
+function useInterval(callback, delay, index) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay, index]);
+}
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const [indexPrc, setIndexPrc] = useState(0);
+  const [slide, setSlide] = useState(0);
+
+  const handleArrow = (nr) => {
+    if (index === 0 && nr < index) {
+      setIndex(5);
+    } else if (index === 5 && nr > index) {
+      setIndex(0);
+    } else setIndex(nr);
+    setIndexPrc(index);
+    setSlide(0);
+  };
+
+  useInterval(
+    () => {
+      if (slide < 100) setSlide(slide + 1);
+      else if (slide === 100) handleArrow(index + 1);
+    },
+    50,
+    index
+  );
+
+  // console.log(slide);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={
+        `flex min-h-screen flex-col items-center bg-c1 text-c2 font-normal overflow-x-hidden py-32 select-none ` +
+        roboto.className
+      }
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="flex gap-x-3 w-[80vw]">
+        {buttonsUpSlider.map((bttn, i) => {
+          // console.log(`${bttn.icon}${i === index ? ".png" : " (1).png"}`);
+          return (
+            <div
+              key={i}
+              className={
+                "flex relative items-center border border-c3 rounded-[4px] p-[14px_16px] h-[56px] justify-center w-fit text-c4 cursor-pointer"
+              }
+              onClick={() => {
+                setSlide(0);
+                setIndex(i);
+                setIndexPrc(index);
+              }}
+            >
+              <div
+                className={
+                  i === index ? "h-full bg-c3 absolute top-0 left-0" : "hidden"
+                }
+                style={{ width: `${slide}%` }}
+              ></div>
+              {i === 0 || i === 5 ? (
+                <img
+                  src={`${bttn.icon}${i === index ? ".png" : " (1).png"}`}
+                  className=" mr-2 h-[22px] z-20"
+                />
+              ) : (
+                <img
+                  src={`${bttn.icon}${i === index ? ".png" : " (1).png"}`}
+                  className=" mr-2 h-[18px] z-20"
+                />
+              )}
+              <p className={i === index ? "text-white z-20" : "text-c4 z-20"}>
+                {bttn.title}
+              </p>
+            </div>
+          );
+        })}
       </div>
+      <div className="flex justify-center my-10 w-[95vw] h-[550px] relative">
+        <img
+          src="/icons/lefta.png"
+          className="mr-14 cursor-pointer absolute top-[40%] left-4"
+          onClick={() => handleArrow(index - 1)}
+        />
+        {sliderElements.map((se, i) => {
+          if (i !== index) return <div key={i}></div>;
+          return (
+            <motion.div
+              initial={{ x: -80, opacity: 0 }}
+              animate={{ x: 0, opacity: 1, transition: { duration: 0.5 } }}
+              key={i}
+              className={
+                index === i
+                  ? "grid grid-cols-[1fr_2fr] w-[80%]  h-full gap-x-[3vw]"
+                  : "hidden"
+              }
+            >
+              <motion.div className="w-[352px] h-full flex flex-col container1">
+                <h2 className=" text-[30px] mb-5 leading-[36px]">{se.title}</h2>
+                <p className="text-[18px] text-[#A3A3B1] leading-[26px]">
+                  {se.desc}
+                </p>
+                <p className="text-[#71717E] mt-2">Replaces:</p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+                <div className="flex gap-x-3 mb-4 mt-4">
+                  <img src="/grey/g3.png" />
+                  <img src="/grey/g2.png" />
+
+                  <img src="/grey/g1.png" />
+                </div>
+                <button className="border border-c4 py-3 w-fit px-[4vw] flex justify-center items-center rounded-[4px] font-bold text-c4">
+                  Explore IDE features{" "}
+                  <i className="fa-sharp fa-solid fa-chevron-right ml-1 text-xs "></i>
+                </button>
+              </motion.div>
+              <motion.div className="w-full h-full flex items-center justify-center shadow-[0_80px_150px_#00000080] container2">
+                <img src={se.img} className="" />
+              </motion.div>
+            </motion.div>
+          );
+        })}
+        <img
+          src="/icons/righta.png"
+          className="ml-14 cursor-pointer absolute top-[40%] right-4"
+          onClick={() => handleArrow(index + 1)}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
